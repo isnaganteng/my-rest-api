53b22ba71389e941bba6038f5fbc83279abc8c8e
isnaganteng <123642389+isnaganteng@users.noreply.github.com>
2023-12-29 14:17:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'ISNA GANTENG'// yourname
 global.MONGO_DB_URI = "mongodb+srv://isnaganteng:isnaganteng@nothing.pi41fk0.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "isnaturu@gmail.com" //email
